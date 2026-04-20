17d210f2c33e56777624135a33400f687721d683
Kaan Ilgin <kaanilgin64@gmail.com>
2026-04-20 14:49:13 +0300

    Auth callback sayfası eklendi

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@/components/lib/supabase";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams, origin } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  if (code) {
+    const supabase = createClient();
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(`${origin}/dashboard`);
+}
